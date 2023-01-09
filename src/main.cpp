cbc2247cbe77c9663a7b9f6c748b0d9bc8f79d55
tigerros <tigerros.gh@gmail.com>
2023-01-09 11:52:43 +0100

    Added methods and improved "intro"

--- src/main.cpp
+++ src/main.cpp
@@ -6,16 +6,19 @@ const unsigned char PIND_PIEZO = 11;
 const float TEMPERATURE_ANALOG_TO_CELSIUS = (5.0F / 1024.0F) * 100;
 const int MAX_CELSIUS = 40;
 const int MIN_CELSIUS = 0;
+const unsigned long DURATION_BETWEEN_TONES = 200;
 const int LED_PINS_LENGTH = 4;
 const unsigned char LED_PINS[LED_PINS_LENGTH] = {3, 5, 9, 10 };
-unsigned int LED_PINS_CELSIUS_TRIGGERS[LED_PINS_LENGTH + 1] = {};
+int LED_PINS_CELSIUS_TRIGGERS[LED_PINS_LENGTH + 1] = {};
 
 bool isOn = false;
 bool wasOn = false;
 bool wasButtonPressed = false;
 
-void turnOffLights();
-void adjustLights(int);
+void turnOffLEDs();
+// Adjusts all the LEDs depending on the analog out temperature.
+// The first LEDs will be turned on if the celsius is higher than MIN_CELSIUS
+void adjustLEDs(int temperatureAnalog);
 
 void setup() {
     Serial.begin(9600);
@@ -29,10 +32,17 @@ void setup() {
 
         if (i < LED_PINS_LENGTH) {
             analogWrite(LED_PINS[i], 255);
-            //tone(PIND_PIEZO, (i + 1) * 500, 250);
-            delay(250);
+            tone(PIND_PIEZO, (i + 1) * 500, DURATION_BETWEEN_TONES);
+            delay(DURATION_BETWEEN_TONES);
             analogWrite(LED_PINS[i], 0);
         }
+    }
+
+    for (int i = LED_PINS_LENGTH - 1; i >= 0; i--) {
+        analogWrite(LED_PINS[i], 255);
+        tone(PIND_PIEZO, (i + 1) * 500, DURATION_BETWEEN_TONES);
+        delay(DURATION_BETWEEN_TONES);
+        analogWrite(LED_PINS[i], 0);
     }
 }
 
@@ -45,38 +55,41 @@ void loop() {
     }
 
     if (isOn) {
-        adjustLights(analogRead(PINA_TEMPERATURE));
+        adjustLEDs(analogRead(PINA_TEMPERATURE));
     } else if (wasOn) {
-        turnOffLights();
+        turnOffLEDs();
     }
 
     wasOn = isOn;
     wasButtonPressed = isButtonPressed;
 }
 
-void turnOffLights() {
+float temperatureAnalogToCelsius(int temperatureAnalog) {
+    return (temperatureAnalog * TEMPERATURE_ANALOG_TO_CELSIUS) - 50.0F;
+}
+
+int ledPWM(float celsius, int ledIndex) {
+    if ((ledIndex < LED_PINS_LENGTH - 1) && (celsius >= LED_PINS_CELSIUS_TRIGGERS[ledIndex + 1])) {
+        return 255;
+    }
+
+    int triggerDiff = LED_PINS_CELSIUS_TRIGGERS[ledIndex + 1] - LED_PINS_CELSIUS_TRIGGERS[ledIndex];
+
+    return ((celsius - LED_PINS_CELSIUS_TRIGGERS[ledIndex]) * 255.0F) / triggerDiff;
+}
+
+void turnOffLEDs() {
     for (unsigned char i : LED_PINS) {
         analogWrite(i, 0);
     }
 }
 
-void adjustLights(int temperatureAnalog) {
-    float celsius = (temperatureAnalog * TEMPERATURE_ANALOG_TO_CELSIUS) - 50.0F;
+void adjustLEDs(int temperatureAnalog) {
+    float celsius = temperatureAnalogToCelsius(temperatureAnalog);
 
     for (int i = 0; i < LED_PINS_LENGTH; i++) {
         if (celsius >= LED_PINS_CELSIUS_TRIGGERS[i]) {
-            if (i < LED_PINS_LENGTH - 1 && celsius >= LED_PINS_CELSIUS_TRIGGERS[i + 1]) {
-                analogWrite(LED_PINS[i], 255);
-                continue;
-            }
-
-            float analog = (celsius - LED_PINS_CELSIUS_TRIGGERS[i]) * 255 / (LED_PINS_CELSIUS_TRIGGERS[i + 1] - LED_PINS_CELSIUS_TRIGGERS[i]);
-            Serial.print(i);
-            Serial.print(": ");
-            Serial.print(celsius);
-            Serial.print(" ");
-            Serial.println(analog);
-            analogWrite(LED_PINS[i], analog);
+            analogWrite(LED_PINS[i], ledPWM(celsius, i));
         } else {
             analogWrite(LED_PINS[i], 0);
         }
